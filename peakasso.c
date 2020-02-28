25c950fadf742f8a9b2233cc87073777b03e2602
msaidzengin <m.zengin@etu.edu.tr>
2020-02-28 23:55:28 +0300

    code compose is added

--- peakasso.c
+++ peakasso.c
@@ -13,19 +13,30 @@ int main() {
         scanf("%c%c", &dump1, &dump2);
     }
 
-    for( int i = 0; i < sizeof(canvas)/sizeof(canvas[0]); i++) {
-        for (int j = 0; j < sizeof(canvas[i])/sizeof(canvas[i][0]); j++) {
-            printf("%c", canvas[i][j]);
-        }
-        printf("\n");
-    }
+    int rectangles[3][4] = {
+        {1, 1, 5, 1},
+        {3, 2, 7, 2},
+        {5,5,2,4}
+    };
+    int rectSize = sizeof(rectangles)/sizeof(rectangles[0]);
 
-    printf("PROGRAM mypeakasso;\n");
+    printf("PROGRAM mypeakasso; !! The number of PAINT-CANVAS statements is %d\n", rectSize);
     printf("CANVAS-INIT-SECTION :\n");
-    printf("CONST CanvasX = %d ; CONST CanvasY = %d ; CursorX = 1 ; CursorY = 1 ;\n", x, y);
-    printf("BRUSH-DECLARATION-SECTION :\n");
-    printf("BRUSH b1 = 5 1, b2= 7 2;");  // brushlar belirlenip for loop ile yazılacak.
-    
+    printf("CONST CanvasX = %d ; CONST CanvasY = %d ; CursorX = %d ; CursorY = %d ;\n", x, y, rectangles[0][0], rectangles[0][1]);
+    printf("BRUSH-DECLARATION-SECTION : !! Declare brushes\n");
+    printf("BRUSH ");
+    for(int i = 0; i < rectSize; i++) {
+        if (i == rectSize - 1) printf("b%d = %d %d; \n\n", (i+1), rectangles[i][2], rectangles[i][3]);
+        else printf("b%d = %d %d, ", (i+1), rectangles[i][2], rectangles[i][3]);
+    }
+    printf("DRAWING-SECTION : !! Start drawing\n");
+    printf("PAINT-CANVAS b1;\n");
+    for(int i = 1; i < rectSize; i++) {
+        printf("MOVE CursorX TO %d ;\n", rectangles[i][0]);
+        printf("MOVE CursorY TO %d ;\n", rectangles[i][1]);
+        printf("PAINT-CANVAS b%d;\n", (i+1));
+    }
+    printf("EXHIBIT-CANVAS;\n");
 
     return 0;
 }
